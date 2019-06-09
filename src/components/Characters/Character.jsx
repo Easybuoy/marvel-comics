46cad418ec6a9ac8b5e8c42dd4df1c4d25e2ed20
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-06-09 19:04:52 +0100

    setup preloader

--- src/components/Characters/Character.jsx
+++ src/components/Characters/Character.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 export default function Characters({ character }) {
   const { name, thumbnail, description } = character;
-  console.log(character);
+  // console.log(character);
   let imageUrl = `${thumbnail.path}/detail.${thumbnail.extension}`;
 
+  const trimWord = string => {
+    if (!string) {
+      return "No Description available for this character.";
+    }
+
+    return `${string.substring(0, 40)}...`;
+  };
   return (
-    <div className="col-lg-3 col-md-3 col-sm-12 mb-4">
+    <div className="col-lg-3 col-md-4 col-sm-12 mb-4">
       <div className="card ">
         <div className="view overlay">
           <img
@@ -23,7 +30,7 @@ export default function Characters({ character }) {
         <div className="card-body">
           <h4 className="card-title">{name}</h4>
 
-          <p className="card-text">{description}</p>
+          <p className="card-text">{trimWord(description)}</p>
 
           <a href="##" className="btn btn-outline-secondary waves-effect">
             More
