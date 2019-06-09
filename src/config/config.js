9975ad09d19ef59b7c8b7bbdac0f641a377a63ff
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-06-09 08:51:10 +0100

    setup url configuration for making network requests

---
+++ src/config/config.js
@@ -0,0 +1,21 @@
+import md5 from "md5";
+import dotenv from "dotenv";
+dotenv.config();
+
+const getUrlDetails = () => {
+  let baseUrl = "http://gateway.marvel.com";
+  let timeStamp = new Date().getTime();
+  let privateKey = process.env.REACT_APP_PRIVATE_KEY;
+  let publicKey = process.env.REACT_APP_PUBLIC_KEY;
+  let combinedKeys = `${timeStamp}${privateKey}${publicKey}`;
+  let hash = md5(combinedKeys);
+
+  return {
+    baseUrl,
+    timeStamp,
+    publicKey,
+    hash
+  };
+};
+
+export { getUrlDetails };
